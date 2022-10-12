d9ab7a04cc70b8ef7b08808cac2bb6d2e96fcdaf
taetaehoo <hapkldo09@naver.com>
2022-10-12 15:52:14 +0900

    dbConnection

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mysql = require('mysql');
+const dbconfig = require('./config/database.js');
+const connection = mysql.createConnection(dbconfig);
+
+connection.connect();
+
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', (req, res) => {
+    res.send('Root');
+})
+
+app.get('/users', (req, res) => {
+    connection.query('SELECT * from Users', (error, rows) => {
+        if (error) throw error;
+        console.log('User info is: ', rows);
+        res.send(rows);
+    })
+});
+
+app.listen(3000, () => console.log('3000번 포트에서 대기중'));
---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+    host: 'localhost',
+    user: 'root',
+    password: 'rkdxogur123!',
+    database: 'my_db'
+}
