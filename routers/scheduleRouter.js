db6dbaef8da15d1f017789e1372cf5c5aceae02c
taetaehoo <hapkldo09@naver.com>
2022-11-09 20:24:16 +0900

    added tuning

--- routers/scheduleRouter.js
+++ routers/scheduleRouter.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const connection = require("../config/mysql");
 
+const getScheduleIndex = (time) => {
+    const times = time.split(":");
+    const index = parseInt(times[0]) * 2;
+
+    return parseInt(times[1]) == 30 ? index + 2 : index + 1;
+}
+
 router.get("/", async (req, res) => {
     await connection.query(`Select * from schedule`, (err, rows) => {
         if (err) throw err;
