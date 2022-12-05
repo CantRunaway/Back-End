d25b7cd950b4b132c06d2666a7b6300927c47ab0
taetaehoo <hapkldo09@naver.com>
2022-12-05 21:12:43 +0900

    빡쳐

--- routers/commuteRouter.js
+++ routers/commuteRouter.js
@@ -20,4 +20,23 @@ router.get("/:user_id", async(req, res) => {
     }
 })//내가 출근 상태일때 퇴근으로 바꾸면서 출근 찍은 시간하고 비교하기 위한 선행 쿼리
 
+
+router.post("/insertCommute", async(req, res) => {
+    const {user_id, work_type_index, start_time, end_time} = req.body;
+    const connection = await pool.getConnection();
+    try {
+        await connection.beginTransaction();
+
+        const result = await connection.query(`call Diligence('${user_id}', '${work_type_index}', '${start_time}')`);
+        const result2 = await connection.query(`call Laziness('${user_id}', '${work_type_index}', '${end_time}')`);
+
+        await connection.commit();
+        return res.status(restStatus.success).json([[result], [result2]]);
+    }catch(err) {
+        await connection.rollback();
+        return res.status(restStatus.fail).json(err);
+    }finally {
+        connection.release();
+    }
+})
 module.exports = router;
